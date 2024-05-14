0b33bdcb503382e682f80caeaf8c14473d701d28
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-14 07:37:22 +0000

    Tue, May 14, 2024, 3:37 PM +08:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,7 +1,7 @@
 #collect input
 source=input()
 #replace
-target=source.replace(':)', 'U0001f600')
+target=source.replace(':)', '\U0001f600').replace(':(', ')
 #print output with emoji
 
 print(target)
