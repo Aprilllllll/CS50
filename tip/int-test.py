f8a8d9912ff57f9a2e1b53b99e1d136e29835dc3
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-19 14:03:45 +0000

    Sun, May 19, 2024, 10:03 PM +08:00

--- tip/int-test.py
+++ tip/int-test.py
@@ -1,5 +1,5 @@
-percent = input("What percentage would you like to tip? ")
-percent = percent.replace('%','')
+percent_string = input("What percentage would you like to tip? ")
+percent_float = percent_string.strip('%')
 percent = int("{:.2%}".format(percent))
 
 print(percent)
