5ec8102d7a2e2e94f87a2a84eed98aaee0ba403c
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-19 14:23:39 +0000

    Sun, May 19, 2024, 10:23 PM +08:00

--- tip/tip.py
+++ tip/tip.py
@@ -5,7 +5,9 @@ def main():
     print(f"Leave ${tip:.2f}")
 
 def dollars_to_float(d):
-    
+    dollars_input = d
+    dollars_clean = dollars_input.replace('$','')
+    dollars_to_float = int(dollars_clean)
 
 #def dollars_to_float(d):
     #dollars_input = input("How much was the meal? ")
@@ -14,7 +16,7 @@ def dollars_to_float(d):
 
 
 def percent_to_float(p):
-    percent_string = input("What percentage would you like to tip? ")
+    percent_string = p
     percent_string_clean = percent_string.strip('%')
     percent_int = int(percent_string_clean)
     percent_to_float = percent_int/100
