161e7529e01e8b5c06b73c277a72858b6591a707
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-22 10:41:07 +0000

    Wed, May 22, 2024, 6:41 PM +08:00

---
+++ deep/deep.py
@@ -0,0 +1,4 @@
+answer = input("What is the answer to the Great Question of Life, the Universe and Everything?")
+
+if
+
--- tip/int-test.py
+++
@@ -1,6 +0,0 @@
-percent_string = input("What percentage would you like to tip? ")
-percent_string_clean = percent_string.strip('%')
-percent_int = int(percent_string_clean)
-percent_float = percent_int/100
-
-print(percent_float)
