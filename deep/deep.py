9906f5fb77b52659a90df50dd5f6251030982d96
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-22 10:52:55 +0000

    Wed, May 22, 2024, 6:52 PM +08:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,7 @@
 answer = input("What is the answer to the Great Question of Life, the Universe and Everything?")
 
-match answer:
+case_insensitive_answer=answer.lower()
+match case_insensitive_answer:
     case "42" | "forty two" | "forty-two":
         print("Yes")
 
