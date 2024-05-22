76968929371b9e75a7e6121918326b5f4245bdc3
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-22 11:05:32 +0000

    Wed, May 22, 2024, 7:05 PM +08:00

--- deep/deep.py
+++ deep/deep.py
@@ -3,7 +3,7 @@ user_input_answer = input("What is the answer to the Great Question of Life, the
 clean_string=user_input_answer.strip().lower()
 
 
-match no_space_string:
+match clean_string:
     case "42" | "forty two" | "forty-two":
         print("Yes")
 
