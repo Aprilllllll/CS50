5a9275b045f9a4523a08c7ba5affc766aeb91193
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-22 11:18:36 +0000

    Wed, May 22, 2024, 7:18 PM +08:00

--- bank/bank.py
+++ bank/bank.py
@@ -3,5 +3,11 @@ user_greeting=input("Greeting")
 #convert to clean string
 clean_string=user_greeting.strip().lower()
 #do match
+if clean_sting=="hello":
+    print("$100")
+    elif clean_string[0]=="h":
+        print("$20")
+else:
+    print
 
 #pring result
