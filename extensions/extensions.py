0deb4584285904f40a296883d59c2dca88da3b8e
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-24 07:17:01 +0000

    Fri, May 24, 2024, 3:17 PM +08:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,5 +1,20 @@
 #clear input to all lowercase string
 user_input=input("File name:")
-clean_string=user_input.strip().lowercase()
-extension=clean_string
+
+
+
+#only leave extension name
+clean_string=user_input.strip().lower()
+
+def remove_before_dot(t, s):
+    before, sep, after = t.partition(s)
+    return after
+
+symbol="."
+
+extension=remove_before_dot(clean_string, ".")
+
+print(extension)
 #do match
+
+
