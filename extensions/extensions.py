a1c29acff3ec77cf9047ba3c9df1eda4c01c85a9
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-24 07:18:04 +0000

    Fri, May 24, 2024, 3:18 PM +08:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,18 +3,16 @@ user_input=input("File name:")
 
 
 
-#only leave extension name
+#function that take extension name
 clean_string=user_input.strip().lower()
 
 def remove_before_dot(t, s):
     before, sep, after = t.partition(s)
     return after
 
-symbol="."
-
+#match extension
 extension=remove_before_dot(clean_string, ".")
 
-print(extension)
-#do match
+if 
 
 
