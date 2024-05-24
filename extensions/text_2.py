3abd4c843fddc1cff305cfce3de12cc704335ce6
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-24 06:56:16 +0000

    Fri, May 24, 2024, 2:56 PM +08:00

--- extensions/text_2.py
+++ extensions/text_2.py
@@ -1,10 +1,7 @@
+
+
 def remove_before_symbol(text, symbol):
     before, sep, after = text.partition(symbol)
 
-# 示例字符串
-text = "Hello, world! How are you?"
-
-# 要删除的符号之前的内容
-symbol = '!'
 
 print(before)
