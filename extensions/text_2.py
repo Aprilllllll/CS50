d6b97122e7bcd772449de55d2a906500f8535cc4
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-24 07:04:33 +0000

    Fri, May 24, 2024, 3:04 PM +08:00

--- extensions/text_2.py
+++ extensions/text_2.py
@@ -3,9 +3,11 @@
 input_sentence=input("sentence?")
 input_symbol=input("symbol?")
 
-result=remove_before_symbol(input_sentence,input_symbol)
+
 def remove_before_symbol(t, s):
     before, sep, after = t.partition(s)
     return before
+
+result=remove_before_symbol(input_sentence,input_symbol)
 
 print(result)
