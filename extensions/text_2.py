0826179d9e8124e7f319f2f76949e9826abd9b00
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-24 06:54:01 +0000

    Fri, May 24, 2024, 2:54 PM +08:00

--- extensions/text_2.py
+++ extensions/text_2.py
@@ -1,9 +1,5 @@
 def remove_before_symbol(text, symbol):
     before, sep, after = text.partition(symbol)
-    if sep:  # 检查分隔符是否存在
-        return sep + after
-    else:
-        return text
 
 # 示例字符串
 text = "Hello, world! How are you?"
