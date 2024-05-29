f0cee36999a3a87b12ac173ed4be2d60fc41d685
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-29 07:47:06 +0000

    Wed, May 29, 2024, 3:47 PM +08:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,12 +1,12 @@
-inputtime=input("What time is it?")
+inputtime=input("What time is it? ")
 
 def main(converted_time):
     if 7.00 <= converted_time <= 8.00:
-    print("breakfast time")
+        print("breakfast time")
     if 12.00 <= converted_time <= 13.00:
-    print("lunch time")
+        print("lunch time")
     if 18.00 <= converted_time <= 19.00:
-    print("dinner time")
+        print("dinner time")
 
 def convert(time):
     hour, minute = time.split(":")
