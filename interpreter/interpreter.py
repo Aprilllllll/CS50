8cfe6ebbd73ec4fe2003c67ef2cf99914eb29785
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-29 04:50:35 +0000

    Wed, May 29, 2024, 12:50 PM +08:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -4,6 +4,11 @@ express_string=input("Expression: ")
 #split string
 fomula_set=express_string.split(" ")
 
-def calculator(x, y, z)
-    
+#def calculator(x, y, z)
+
 #convert
+x=fomula_set[1]
+z=fomula_set[-1]
+y=fomula_set[2]
+
+print(y)
