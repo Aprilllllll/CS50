aac45f89dae5c5ffe51a46c7a8ccba32562950ea
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-05-29 07:25:00 +0000

    Wed, May 29, 2024, 3:25 PM +08:00

--- meal/meal.py
+++ meal/meal.py
@@ -5,7 +5,8 @@ inputtime="7:50"
 def convert(time):
     hour, minute = time.split(":")
     convert_minute = float(int(minute)/60)
-    return hour + str(convert_minute)
+    return hour+str(convert_minute)
+#    return hour
 
 result=convert(inputtime)
 print(result)
