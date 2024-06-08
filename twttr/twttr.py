350a44da204ca4f508af1eca6bda5723d9add823
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-06-08 11:33:32 +0000

    Sat, Jun 8, 2024, 7:33 PM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,9 @@
 str_input = str(input(""))
-remove_vowels = "aeiouAEIOU"
+
 
 def rm_vowels(str_input):
+    remove_vowels = "aeiouAEIOU"
+    return_word = ""
     for a in str_input:
         if a not in remove_vowels:
             return_word += a
