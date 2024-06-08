48560119fe1e4b0cec4870393725bb8137b4cf5b
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-06-08 13:17:11 +0000

    Sat, Jun 8, 2024, 9:17 PM +08:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,9 +8,10 @@ def main():
 
 def is_valid(s):
     valid_count = 0
-    result_count =
+    result_count = 0
     while valid_count = 0:
         if s[0:1].isalpha():
+            result_count += valid_count
 
 
 
