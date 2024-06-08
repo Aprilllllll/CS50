c27983fa261ede008a238ae947df3e90813f2bfb
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-06-08 10:46:23 +0000

    Sat, Jun 8, 2024, 6:46 PM +08:00

---
+++ coke/coke.py
@@ -0,0 +1,14 @@
+def coke_machine():
+    total_paid = 0
+    while total_paid < 50:
+      coin = int(input("Insert Coin: "))
+      if coin in [5, 15, 20]:
+        total_paid += coin
+        if total_paid < 50:
+            print(f"Amount Due: {50 - total_paid}")
+      else:
+        print(f"Wrong coin, amount Due: {50 - total_paid}")
+
+    if total_paid > 50:
+       print(f"Change Owed: {total_paid - 50}")
+coke_machine()
