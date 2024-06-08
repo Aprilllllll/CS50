69fae6d38c6061932d6155dce0728e3ec427ac4f
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-06-08 11:15:51 +0000

    Sat, Jun 8, 2024, 7:15 PM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,9 +1,9 @@
 str_input = input("")
 
 def rm_vowels():
-    for a in str_input:
+    for a in str(str_input):
         if a in ['a''e''i''o''u''A''E''I''O''U']:
-            print('', end="")
+            print('_', end="")
         else:
             print(a, end="")
 
