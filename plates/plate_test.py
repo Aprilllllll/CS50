c62e092a5befa619f5e2b167883f8a6b54b7bf85
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-06-09 13:54:53 +0000

    Sun, Jun 9, 2024, 9:54 PM +08:00

---
+++ plates/plate_test.py
@@ -0,0 +1,36 @@
+import string
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    result_count = 0
+    while result_count < 5:
+        if s[0:1].isalpha():
+            result_count += 1
+        if 2 <= len(s) <= 5:
+            result_count += 1
+        #if len(s) > 2:
+        if s[1:-1].isalpha():
+                result_count += 1
+        if punctuation_check(s):
+            result_count +=1
+    if result_count == 4:
+        #print(result_count)
+        return True
+
+def punctuation_check(check_context):
+    for char in check_context:
+        if char in string.punctuation:
+            return True
+
+
+
+
+
+
+main()
