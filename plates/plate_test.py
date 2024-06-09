e87b06c7c9e8bc1b55f8752f09db5497050a6ed0
Aprilllllll <96217475+Aprilllllll@users.noreply.github.com>
2024-06-09 14:08:22 +0000

    Sun, Jun 9, 2024, 10:08 PM +08:00

--- plates/plate_test.py
+++ plates/plate_test.py
@@ -9,7 +9,7 @@ import string
 
 def is_valid(s):
     result_count = 0
-    while result_count < 5:
+    while result_count < 4:
         if s[0:1].isalpha():
             result_count += 1
             print("start with at least 2 letters")
@@ -23,13 +23,12 @@ def is_valid(s):
         if punctuation_check(s):
             result_count +=1
             print("no punctuation")
-    if result_count == 4:
         print(result_count + "All Passed")
 
 def punctuation_check(check_context):
     for char in check_context:
         if char in string.punctuation:
             return True
-        
+
 input_plate = input("Plate: ")
 is_valid(input_plate)
